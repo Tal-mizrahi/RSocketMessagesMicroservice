17fc9b381647f3e841bd45a5de8eb577cd10a8af
Tal <talmiz99@gmail.com>
2025-01-06 18:30:53 +0200

    Initial commit

---
+++ src/main/kotlin/il/ac/afeka/cloud/rsocketmessagesmicroservice/RSocketMessagesMicroserviceApplication.kt
@@ -0,0 +1,11 @@
+package il.ac.afeka.cloud.rsocketmessagesmicroservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RSocketMessagesMicroserviceApplication
+
+fun main(args: Array<String>) {
+    runApplication<RSocketMessagesMicroserviceApplication>(*args)
+}
---
+++ src/test/kotlin/il/ac/afeka/cloud/rsocketmessagesmicroservice/RSocketMessagesMicroserviceApplicationTests.kt
@@ -0,0 +1,13 @@
+package il.ac.afeka.cloud.rsocketmessagesmicroservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RSocketMessagesMicroserviceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
